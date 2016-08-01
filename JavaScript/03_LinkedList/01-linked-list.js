4e616911af3433d21d4896f714e0728c84b5ddd4
Moss Green <gufeifei@outlook.com>
2016-08-01 15:51:39 +0800

    adding a LinkedList class

---
+++ JavaScript/03_LinkedList/01-linked-list.js
@@ -0,0 +1,20 @@
+function LinkedList(){
+
+	var Node = function(element){
+		this.element = element;
+		this.next = null;
+	}
+
+	var length = 0;
+	var head = null;
+
+	this.append = function(element){};
+	this.insert = function(position, element){};
+	this.removeAt = function(position){};
+	this.remove = function(element){};
+	this.indexOf = function(element){};
+	this.isEmpty = function(){};
+	this.size = function(){};
+	this.toString = function(){};
+	this.print = function(){};
+}
