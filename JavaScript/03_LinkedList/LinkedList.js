214079672ff9954973ac2e792673137df3f0abdd
Moss Green <gufeifei@outlook.com>
2016-08-02 13:45:06 +0800

    fixing bugs with Linkedlist and texting linkedList

--- JavaScript/03_LinkedList/LinkedList.js
+++ JavaScript/03_LinkedList/LinkedList.js
@@ -39,121 +39,123 @@ function LinkedList(){
 
 	this.insert = function(position, element){
 
-		if(position < 0 || position > length){
-			return false;
+        //check for out-of-bounds values
+        if (position >= 0 && position <= length){
 
-		}else{
-			var node = new Node(element);
-			var current = head;
-			var previous;
-			index = 0;
+        	var node = new Node(element),
+        	current = head,
+        	previous,
+        	index = 0;
 
-			/*if position is 0, make the first one to the second (node.next),
-			and make the node to be the head.*/
-			if(position === 0){
+            if (position === 0){ //add on first position
 
-				node.next = current;
-				head = node;
+            	node.next = current;
+            	head = node;
 
-			}else{
+            } else {
+            	while (index++ < position){
+            		previous = current;
+            		current = current.next;
+            	}
+            	node.next = current;
+            	previous.next = node;
+            }
 
-				current = head;
+            length++; //update size of list
 
-				while(index ++ < position){
-					previous = current;
-					current = current.next;
-				}
+            return true;
 
-				/*while (index == position), let node = privious
-				which means the current node*/
-				node.next = current;
-				previous.next = node;
-			}
+        } else {
+        	return false;
+        }
+    };
 
-			length ++;
 
-			return true;
-		}
+    this.removeAt = function(position){
+
+        //check for out-of-bounds values
+        if (position > -1 && position < length){
+
+        	var current = head,
+        	previous,
+        	index = 0;
+
+            //removing first item
+            if (position === 0){
+            	head = current.next;
+            } else {
+
+            	while (index++ < position){
+
+            		previous = current;
+            		current = current.next;
+            	}
+
+                //link previous with current's next - skip it to remove
+                previous.next = current.next;
+            }
+
+            length--;
+
+            return current.element;
+
+        } else {
+        	return null;
+        }
+    };
+
+    this.remove = function(element){
+
+    	var index = this.indexOf(element);
+    	return this.removeAt(index);
+    };
+
+    this.indexOf = function(element){
+
+    	var current = head,
+    	index = 0;
+
+    	while (current) {
+    		if (element === current.element) {
+    			return index;
+    		}
+    		index++;
+    		current = current.next;
+    	}
+
+    	return -1;
+    };
+
+    this.isEmpty = function(){
+
+    	return length ===0;
+    };
+
+    this.size = function(){
+    	return length;
+
+    };
+
+	//head is private attribute, which means it cannot be modified outside the class
+	this.getHead = function(){
+		return head;
 	};
 
 
-	this.removeAt = function(position){
+    this.toString = function(){
 
-		if(position < 0 || position > length){
-			return null;
-		}else{
+        var current = head,
+            string = '';
 
-			var current = head;
-			var previous;
-			var index = 0;
+        while (current) {
+            string = current.element;
+            current = current.next;
+        }
+        return string;
 
-			if(position === 0){
-				head = current.next;
-			}else{
-
-				while(index ++ < position){
-					previous = current;
-					current = current.next;
-				}
-
-				/*this removes the element after the previous and before current.next
-				which means the current element*/
-				previous.next = current.next;
-			}
-			length --;
-			return  current.element;
-		}
-
+    };
+    
+	this.print = function(){
+		console.log(this.toString());
 	};
-	
-	this.remove = function(element){
-		var index = indexOf(element);
-		return removeAt(inde);
-	};
-
-	this.indexOf = function(element){
-
-		var current = head;
-		var index = -1;
-
-		while (current){
-			if(element == current.element){
-				return index;
-			}
-
-			index++;
-			current = current.next;
-		}
-
-		return -1;
-	};
-
-	this.isEmpty = function(){
-
-		return length ===0;
-	};
-
-	this.size = function(){
-		return length;
-
-	};
-
-//head is private attribute, which means it cannot be modified outside the class
-this.getHead = function(){
-	return head;
-};
-
-
-this.toString = function(){
-
-	var current = head;
-	var string = '';
-
-	while(current){
-		string = current.element;
-		current = current.next;
-	}
-	return string;
-};
-this.print = function(){};
 }
