97fd3884afda5912c6f2203a1246fec0ee54dcea
Moss Green <gufeifei@outlook.com>
2016-08-02 14:24:32 +0800

    adding an empty DoublyLinkedList

---
+++ JavaScript/03_LinkedList/DoubleLinkedList.js
@@ -0,0 +1,16 @@
+function DoublyLinkedList(){
+
+	var Node = function(element){
+		this.element = element;
+		this.next = null;
+		this.prev = null;
+	};
+
+	var lenth = 0;
+	var head = null;
+	var tail = null;
+
+	//methods here
+
+
+}
