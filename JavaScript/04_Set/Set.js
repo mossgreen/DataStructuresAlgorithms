43c908d86dedb070c0e20fa71c128724707eb03f
Moss Green <gufeifei@outlook.com>
2016-08-04 15:18:23 +0800

    Adding set class

---
+++ JavaScript/04_Set/Set.js
@@ -0,0 +1,41 @@
+function(){
+	var items = {};
+
+	this.add = function(){};
+
+	this.remove = function(){};
+
+	this.has = function(){};
+
+	this.clear = function(){};
+
+	this.size = function(){};
+
+	this.sizeLegacy = function(){};
+
+	this.values = function(){};
+
+	this.valuesLegacy = function(){};
+
+	this.getItems = function(){};
+
+	this.union = function(){};
+
+	this.intersection = function(){};
+
+	this.difference = function(){};
+
+	this.subset = function(){};	
+
+
+
+
+
+
+
+
+
+
+
+
+}
