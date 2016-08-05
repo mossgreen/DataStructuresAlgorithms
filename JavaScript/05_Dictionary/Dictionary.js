d7e7db26543546d3c25727575f977052adbbf0d6
Moss Green <gufeifei@outlook.com>
2016-08-05 18:21:14 +0800

    adding dictionary class

---
+++ JavaScript/05_Dictionary/Dictionary.js
@@ -0,0 +1,25 @@
+function Dictionary(){
+
+	var items = {};
+
+	this.set = function(key, value){ };
+
+	this.remove = function(key){ };
+
+	this.has = function(key){ };
+
+	this.get = function(key){ };
+
+	this.clear = function(){};
+
+	this.size = function(){};
+
+	this.keys = function(){};
+
+	this.value = function(){};
+
+	this.each = function(){};
+
+	this.getItems = function(){};
+
+}
