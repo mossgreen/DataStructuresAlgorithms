6666775947ea1359f64df4b719692786406de79e
Moss Green <gufeifei@outlook.com>
2016-08-05 19:14:25 +0800

    adding hash table class

---
+++ JavaScript/05_Dictionary/Hashtable.js
@@ -0,0 +1,17 @@
+function HashTable(){
+	var table = [];
+
+	var lostlostHashCode = function(){};
+
+	var sjb2HashCode = function(){};
+
+	var hashcode = function(){};
+
+	this.put = function(key){};
+
+	this.get = function(key){};
+
+	this.remove = function(key){};
+
+	this.print = function(){};
+}
