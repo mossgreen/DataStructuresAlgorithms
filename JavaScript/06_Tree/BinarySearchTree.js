e48c72923305609f073cf1ec34b110df646ab96a
Moss Green <gufeifei@outlook.com>
2016-08-07 19:33:04 +0800

    adding insert method

--- JavaScript/06_Tree/BinarySearchTree.js
+++ JavaScript/06_Tree/BinarySearchTree.js
@@ -7,9 +7,32 @@ function BinarySearchTree(){
 
 	var root = null;
 
-	this.insert = function(key){};
+	this.insert = function(key){
+		var newNode = new Node(key);
 
-	var insertNode = function(node, newNode){};
+		//special case - first element
+		if(root == null){
+			root = newNode;
+		}else{
+			insertNode(root, newNode);
+		}
+	};
+
+	var insertNode = function(node, newNode){
+		if(newNode.key < node.key){
+			if(node.left === null){
+				node.left = newNode;
+			}else{
+				insertNode(node.left, newNode);
+			}
+		}else{
+			if(node.right === null){
+				node.right = newNode;
+			}else{
+				insertNode(node.right, newNode);
+			}
+		}
+	};
 
 	this.getRoot = function(){};
 
