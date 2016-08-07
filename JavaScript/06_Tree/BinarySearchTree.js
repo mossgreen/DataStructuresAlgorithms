c93d69e9da6a5473db1a203e8ae36dfbacbe6485
Moss Green <gufeifei@outlook.com>
2016-08-07 19:03:36 +0800

    adding BinarySearchTree Class

---
+++ JavaScript/06_Tree/BinarySearchTree.js
@@ -0,0 +1,45 @@
+function BinarySearchTree(){
+	var Node = function(key){
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	};
+
+	var root = null;
+
+	this.insert = function(key){};
+
+	var insertNode = function(node, newNode){};
+
+	this.getRoot = function(){};
+
+	this.search = function(key){};
+
+	var searchNode = function(node, key){};
+
+	this.inOrderTraverse = function(callback){};
+
+	var inOrderTraverseNode = function(node, callback){};
+
+	this.preorderTraverse = function(callback){};
+
+	var preorderTraverseNode = function(node, callback){};
+
+	this.postOrderTraverse = function(callback){};
+
+	var postOrderTraverseNode = function(node, callback){};
+
+	this.min = function(){};
+
+	var minNode = function(node){};
+
+	this.max = function(){};
+
+	var maxNode = function(node){};
+
+	this.remove = function(element){};
+
+	var findMinNode = function(node){};
+
+	var removeNode = function(node, element){};
+}
